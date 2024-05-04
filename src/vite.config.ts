41861a4b2b36fa4ac4ec3cf701a60ab9f97058dd
ZWForum <782552186@qq.com>
2024-05-04 19:08:40 +0800

    init

--- src/vite.config.ts
+++ src/vite.config.ts
@@ -14,5 +14,12 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  server: {
+    host: '0.0.0.0',
+    hmr: true,
+    watch: {
+      usePolling: true
+    }
   }
 })
